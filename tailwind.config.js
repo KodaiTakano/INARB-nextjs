3be2594c04b29cec202aa965853d3ce5715a82dd
Nishino0719 <nishino0719@icloud.com>
2021-11-14 13:30:45 +0900

    💄

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{ts,tsx}'],
   mode: 'jit',
   darkMode: 'media', // 'media' or 'class'
-  theme: { extend: { colors: { 'accent-1': '#333' } } },
+  theme: {
+    extend: { colors: { 'accent-1': '#333' }, width: { '40rem': '40rem' } }
+  },
   variants: { extend: {} },
   plugins: []
 }
